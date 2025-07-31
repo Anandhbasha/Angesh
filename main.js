b29a819c330bcde7ea809d1bd404adf879d2896e
Anandhbasha <anadhakm@gmail.com>
2025-07-31 11:58:47 +0530

    decalration

---
+++ main.js
@@ -0,0 +1,23 @@
+// Hoisting
+// var
+console.log(a);
+
+var a = 10
+var b = 20
+var a = 20
+a=60
+console.log(a+b);
+
+// let
+// console.log(c);
+
+let c = 40
+
+c=50
+console.log(c);
+
+//const
+const d = 60
+d=70
+console.log(d);
+
