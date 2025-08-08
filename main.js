ad9c3b785cb8aa9a69ea9209b21f580a0eb9f327
Anandhbasha <anadhakm@gmail.com>
2025-08-08 11:21:45 +0530

    Data Types

--- main.js
+++ main.js
@@ -1,23 +1,90 @@
-// Hoisting
-// var
-console.log(a);
+// // // Hoisting
+// // // var
+// // console.log(a);
 
-var a = 10
-var b = 20
-var a = 20
-a=60
-console.log(a+b);
+// // var a = 10
+// // var b = 20
+// // var a = 20
+// // a=60
+// // console.log(a+b);
 
-// let
-// console.log(c);
+// // // let
+// // // console.log(c);
 
-let c = 40
+// // let c = 40
 
-c=50
-console.log(c);
+// // c=50
+// // console.log(c);
 
-//const
-const d = 60
-d=70
-console.log(d);
+// //const
+// const d = 60
+// // d=70
+// // console.log(d);
+// // let num1 = 10
+// // scope variable && //local variable
+// { 
+//     let num1 = 10
+//     // console.log(num1);
+// }
+// console.log(num1);
+
+
+// // global variable
+
+// opertors
+    // Arithmetic
+        // +,-,*,/,%,++,--
+        // let num1 = 60
+        let num2 = 80
+        // console.log(num1+num2);
+        // console.log(num2-num1);
+        // console.log(num2/num1);
+        // console.log(num1*num2);
+        // console.log(5%2);
+        // increment
+        // num1 = num1+1
+    //    console.log(num1++);
+    //    console.log(num1);
+    //    console.log(--num1);
+    // //    console.log(num1);
+       
+       
+        
+    // // Comparsion
+    //     // >,<,>=,<=,!=,==,===
+    //     console.log(num1>num2);
+    //     console.log(num1<num2);
+        let num1 = "60"
+        num2 = 60
+        console.log(num1<=num2);
+        console.log(num1>=num2);
+        console.log(num1==num2);
+        console.log(num1===num2);
+        
+        
+    // Logical
+        // &&,||,!
+        // console.log(10<15 && 15>20 && 20<25);
+        // console.log(10>15 || 15>20 || 20<25);
+        console.log(10!=20);
+        
+        
+    // Ternary Opeartor
+    let age = 17
+    console.log(age>=18?"Eligible to Vote":"Not Eligilble");
+    
+// Data types
+// Primitive Data Types
+// number
+let names = "Angesh"
+// string
+// Boolean
+// undefined
+inClass = false
+
+let x
+console.log(typeof(inClass));
+console.log(typeof(names));
+console.log(typeof(age));
+console.log(typeof(x));
 
